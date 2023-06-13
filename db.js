f40410174097d838b7284866b96d26522f5e8bb2
Diegratia <d.febrian2002@gmail.com>
2023-06-13 19:59:13 +0700

    firebase-config

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin')
 const config =  require('./config');
 
-const serviceAccount = require("./key/serviceAccount.json");
+const serviceAccount = require("./key/serviceAccount-firebase-key.json");
 
 const db = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
